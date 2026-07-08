bcd2f6401e3067e16ebd709c43cd94548e60fddf
Arjang Khademi <arjangkhdm@gmail.com>
2026-07-08 10:38:38 +0200

    icons Update

--- app/src/main/java/com/example/projektwohce1_android_appstreetboyz/data/Screen.kt
+++ app/src/main/java/com/example/projektwohce1_android_appstreetboyz/data/Screen.kt
@@ -1,8 +1,10 @@
 package com.example.projektwohce1_android_appstreetboyz.data
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Quiz
+import androidx.compose.material.icons.filled.School
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Route(
@@ -18,16 +20,16 @@ sealed class Route(
     object Flashcards : Route(
         route = "flashcards",
         label = "Lernen",
-        icon = Icons.Default.Star
+        icon = Icons.Default.School
     )
     object Quiz : Route(
         route = "quiz",
         label = "Quiz",
-        icon = Icons.Default.Star
+        icon = Icons.Default.Quiz
     )
     object Quotes : Route (
         route = "quotes",
         label = "Quotes",
-        icon = Icons.Default.Star
+        icon = Icons.Default.FormatQuote
     )
 }
